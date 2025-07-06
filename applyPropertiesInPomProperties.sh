e4ac1f829a62f9cddfba86722a9db346901c8f99
Damineni Sarath Chandra <dis1tra@de.bosch.com>
2025-07-06 23:01:22 +0200

    HPSOFT-12372: Update pom-family.properties with metadata for NCSU

--- applyPropertiesInPomProperties.sh
+++ applyPropertiesInPomProperties.sh
@@ -12,20 +12,20 @@ git clone https://sourcecode.socialcoding.bosch.com/scm/rhp/hp-hyd-config.git ./
 # Clone ref config repo
 git clone https://sourcecode.socialcoding.bosch.com/scm/rhp/hp-ref-config.git ./external/hp-ref-config
 
-for repo in "{$repos[@]}"; do
+for repo in "${repos[@]}"; do
     cd ./external/"$repo"
     for branch in "${branches[@]}"; do
         git checkout "$branch"
         git pull
 
-        git branch feature/HPSOFT-12372-metadata-for-ncsu
-        git checkout feature/HPSOFT-12372-metadata-for-ncsu
+        git branch feature/HPSOFT-12372-metadata-for-ncsu-automatic-"$branch"
+        git checkout feature/HPSOFT-12372-metadata-for-ncsu-automatic-"$branch"
         
         ../../updatePomFamilyProperties.exe display.reboot.time=1000 display.transfer.time=2000 parameter.mode=retain
 
         git add .
         git commit -m "HPSOFT-12372: Update pom-family.properties with metadata for NCSU"
-        git push --set-upstream origin feature/HPSOFT-12372-metadata-for-ncsu
+        git push --set-upstream origin feature/HPSOFT-12372-metadata-for-ncsu-automatic-"$branch"
     done
 done
 
