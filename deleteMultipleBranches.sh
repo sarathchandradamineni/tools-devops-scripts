935a22330b0ba033a1f46068070e5724c1f7fa07
Damineni Sarath Chandra <dis1tra@de.bosch.com>
2025-07-07 10:06:26 +0200

    Updating some more automation scripts

--- deleteMultipleBranches.sh
+++ deleteMultipleBranches.sh
@@ -15,17 +15,9 @@ git clone https://sourcecode.socialcoding.bosch.com/scm/rhp/hp-ref-config.git ./
 for repo in "${repos[@]}"; do
     cd ./external/"$repo"
     for branch in "${branches[@]}"; do
-        git checkout "$branch"
         git pull
 
-        git branch feature/HPSOFT-12372-metadata-for-ncsu-automatic-"$branch"
-        git checkout feature/HPSOFT-12372-metadata-for-ncsu-automatic-"$branch"
-        
-        ../../updatePomFamilyProperties.exe display.reboot.time=1000 display.transfer.time=2000 parameter.mode=retain
-
-        git add .
-        git commit -m "HPSOFT-12372: Update pom-family.properties with metadata for NCSU"
-        git push --set-upstream origin feature/HPSOFT-12372-metadata-for-ncsu-automatic-"$branch"
+        git push origin --delete feature/HPSOFT-12372-metadata-for-ncsu-automatic-"$branch"
     done
     cd ../..
 done
